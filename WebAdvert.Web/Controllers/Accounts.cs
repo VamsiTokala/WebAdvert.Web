fbf90ea534d97fd36de92d521d377a496a97dd2c
vamsi tokala <vamsitokala@live.com>
2022-10-28 14:18:05 +0000

    Setup AWS Cognito identity provider with .net core MVC

---
+++ WebAdvert.Web/Controllers/Accounts.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAdvert.Web.Controllers
+{
+    public class Accounts : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAdvert.Web/Models/Accounts/SignupModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class SignupModel
+    {
+        [Required] //Specifies that a data field value is required.
+        [EmailAddress]// checks email format
+        [Display(Name ="Email")]
+        public string Email { get; set; } //prop tab it will add a public property
+
+        [Required]
+        [DataType(DataType.Password)] // for password type we need to use this
+        [StringLength(6, ErrorMessage = "Password must be at least six checrectors long!")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)] // for password type we need to use this
+        [Compare("password", ErrorMessage ="Password and its confirmation do not match")]
+        [Display(Name = "Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
